b3adac79e9fc766fc74e4df2d795e85ddce94d18
BrunnoFOS <brunnofos@outlook.com>
2024-07-21 23:47:56 -0300

    Adição de Serviços - Versão 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,93 @@
 $(document).ready(function() {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
-    });
+    const secoes = $('section');
+    const itensNav = $('.nav-item');
+    const alturaCabecalho = $('header').outerHeight();
 
-    const sections = $('section');
-    const navItems = $('.nav-item');
-    const headerHeight = $('header').outerHeight();
+    function ItemNavAtivo() {
+        const posicaoScroll = $(window).scrollTop() + alturaCabecalho + 1;
 
-    function updateActiveNavItem() {
-        const scrollPosition = $(window).scrollTop() + headerHeight + 1;
+        let indiceSecaoAtiva = 0;
 
-        let activeSectionIndex = 0;
+        secoes.each(function(i) {
+            const secao = $(this);
+            const topoSecao = secao.offset().top;
+            const fundoSecao = topoSecao + secao.outerHeight();
 
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top;
-            const sectionBottom = sectionTop + section.outerHeight();
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
+            if (posicaoScroll >= topoSecao && posicaoScroll < fundoSecao) {
+                indiceSecaoAtiva = i;
                 return false;
             }
         });
 
-        navItems.removeClass('active');
-        $(navItems[activeSectionIndex]).addClass('active');
+        itensNav.removeClass('active');
+        $(itensNav[indiceSecaoAtiva]).addClass('active');
     }
 
-    $(window).on('scroll', updateActiveNavItem);
-
-    navItems.on('click', function(e) {
+    function CliqueItemNav(e) {
         e.preventDefault();
-        const targetId = $(this).find('a').attr('href');
-        const targetPosition = $(targetId).offset().top - headerHeight;
+        const idAlvo = $(this).find('a').attr('href');
+        const posicaoAlvo = $(idAlvo).offset().top - alturaCabecalho;
 
         $('html, body').animate({
-            scrollTop: targetPosition
+            scrollTop: posicaoAlvo
         }, 80);
 
-        navItems.removeClass('active');
+        itensNav.removeClass('active');
         $(this).addClass('active');
-    });
+    }
 
-    ScrollReveal().reveal('#cta', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
+    function inicializarMenuMobile() {
+        $('#mobile_btn').on('click', function () {
+            $('#mobile_menu').toggleClass('active');
+            $('#mobile_btn').find('i').toggleClass('fa-x');
+        });
+    }
 
-    ScrollReveal().reveal('.dish', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
+    function inicializarScrollReveal() {
+        ScrollReveal().reveal('#cta', {
+            origin: 'left',
+            duration: 2000,
+            distance: '20%'
+        });
 
-    ScrollReveal().reveal('#testimonial_chef', {
-        origin: 'left',
-        duration: 1000,
-        distance: '20%'
-    });
+        ScrollReveal().reveal('.dish', {
+            origin: 'left',
+            duration: 2000,
+            distance: '20%'
+        });
 
-    ScrollReveal().reveal('.feedback', {
-        origin: 'right',
-        duration: 1000,
-        distance: '20%'
-    });
+        ScrollReveal().reveal('#testimonial_chef', {
+            origin: 'left',
+            duration: 1000,
+            distance: '20%'
+        });
 
-    ScrollReveal().reveal('#banner', {
-        origin: 'top',
-        duration: 2000,
-        distance: '30%'
-    });
+        ScrollReveal().reveal('.feedback', {
+            origin: 'right',
+            duration: 1000,
+            distance: '20%'
+        });
 
-    ScrollReveal().reveal('.shape', {
-        origin: 'right',
-        duration: 1000,
-        distance: '20%'
-    });
+        ScrollReveal().reveal('#banner', {
+            origin: 'top',
+            duration: 2000,
+            distance: '30%'
+        });
+
+        ScrollReveal().reveal('.shape', {
+            origin: 'right',
+            duration: 1000,
+            distance: '20%'
+        });
+    }
+
+    function inicializar() {
+        inicializarMenuMobile();
+        inicializarScrollReveal();
+
+        $(window).on('scroll', ItemNavAtivo);
+        itensNav.on('click', CliqueItemNav);
+    }
+
+    inicializar();
 });
-
